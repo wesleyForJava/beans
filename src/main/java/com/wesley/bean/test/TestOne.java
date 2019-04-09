aa28fc8eaf94a51dfe4ed67204444cbe6bf98efc
pc <pc@csscis>
2019-04-09 16:40:05 +0800

    update...

--- src/main/java/com/wesley/bean/test/TestOne.java
+++ src/main/java/com/wesley/bean/test/TestOne.java
@@ -1,12 +1,8 @@
 package com.wesley.bean.test;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.wesley.bean.biz.IBic;
 import com.wesley.bean.biz.impl.IBicImpl;
